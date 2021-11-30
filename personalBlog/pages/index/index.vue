8988b113c8b19c8ff3e1c7866bf5d392acd1f8f1
yangpengfei <2294403566@qq.com>
2021-11-30 23:39:33 +0800

    创建登录页面

--- personalBlog/pages/index/index.vue
+++ personalBlog/pages/index/index.vue
@@ -1,8 +1,16 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<p>Login</p>
+		<view class="login">
+			<uni-forms :modelValue="formData">
+				<uni-forms-item label="姓名" name="name">
+					<input type="text" v-model="formData.username" placeholder="请输入姓名" value="" />
+				</uni-forms-item>
+				<uni-forms-item required name="hobby" label="密码">
+					<input type="text" v-model="formData.password" placeholder="请输入密码" value="" />
+				</uni-forms-item>
+			</uni-forms>
+			<button class="loginBtn" @click="submitForm">登录</button>
 		</view>
 	</view>
 </template>
@@ -11,14 +19,31 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				formData: {
+					username: '',
+					password: ''
+				}
+
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			submitForm(){
+				if (this.formData.username == 'mavengo' && this.formData.password == 123456) {
+					uni.navigateTo({
+						url:'/pages/notes/notes'
+					})
+				} else{
+					uni.showToast({
+					    title: '账号或密码错误！',
+					    duration: 2000,
+						icon:'error',
+					});
+				}
+			}
 		}
 	}
 </script>
@@ -29,24 +54,41 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		height: 94vh;
+		background: url(../../static/index_bgm.jpg) center/cover;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	p {
+		padding: 50rpx;
+		font-size: 89rpx;
+		font-weight: 900;
+		color: #1e5167;
 	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.login {
+		padding: 20rpx;
+		box-sizing: border-box;
+		width: 90%;
+		height: 200px;
+		background-color: #FFFFFF;
+		border-radius: 20rpx;
+		box-shadow: 0 0 20rpx rgb(23, 45, 67);
 	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	
+	input {
+		padding: 0 20rpx;
+		box-sizing: border-box;
+		width: 90%;
+		height: 90rpx;
+		line-height: 90rpx;
+		border: 1px solid rgb(95, 95, 84);
+		box-shadow: 0 0 20rpx rgb(53, 72, 103);
+		border-radius: 20rpx;
+	}
+	
+	.loginBtn {
+		width: 80%;
+		background-color: #007AFF;
+		border-radius: 20rpx;
 	}
 </style>
---
+++ personalBlog/pages/notes/notes.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		nodes界面
+		<button type="default" @click="goback()">返回登录页面</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			goback(){
+				uni.navigateTo({
+					url:'/pages/index/index'
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
